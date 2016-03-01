43918840e0f6e7f926a3205c0de8ea7c5172730e
John Stevens <johnbrycestevens@Johns-MacBook-Pro.local>
2016-03-01 14:12:10 -0600

    Second commit

---
+++ README.md
@@ -0,0 +1,4 @@
+my-personal-website
+===================
+
+A Symfony project created on February 26, 2016, 4:57 pm.
---
+++ src/AppBundle/Controller/HomeController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeController extends Controller
+{
+	/**
+     * @Route("/home", name="app_home")
+     */
+	public function homeAction()
+	{
+		return $this->render('/homepage/home.html.twig');
+
+		return new Response($html);
+	}
+
+	/**
+     * @Route("/projects", name="app_projects")
+     */
+	public function projectsAction()
+	{
+		return $this->render('/homepage/projects.html.twig');
+
+		return new Response($html);
+	}
+
+	/**
+     * @Route("/contact", name="app_contact")
+     */
+	public function contactAction()
+	{
+		return $this->render('/homepage/contact.html.twig');
+
+		return new Response($html);
+	}
+
+	/**
+     * @Route("/blog", name="app_blog")
+     */
+	public function blogAction()
+	{
+		return $this->render('/blogpage/blog.html.twig');
+
+		return new Response($html);
+	}
+}
